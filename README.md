1dccbd52485b53d3cc26c1fcd05d1db48866a31d
Juliana Souza <89201421+julianascs1@users.noreply.github.com>
2021-12-04 17:59:26 -0300

    Adding files

---
+++ README.md
@@ -0,0 +1,32 @@
+# eslint-plugin-miranda-linter
+
+The purpose is to ensure Miranda design system is used in a standardized way
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-miranda-linter`:
+
+```sh
+npm install eslint-plugin-miranda-linter --save-dev
+```
+
+## Usage
+
+Add `miranda-linter` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "miranda-linter"
+    ]
+}
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "plugins": [
+        "react"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 10,
+        "sourceType": "module"
+    },
+    "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/forbid-elements": [1, { 
+                "forbid": [
+                    { "element": "p", "message": "use Miranda <Text> instead" },
+                    { "element": "span", "message": "use Miranda <Text> instead" },
+                    { "element": "b", "message": "use Miranda <Text> instead" },
+                    { "element": "strong", "message": "use Miranda <Text> instead" },
+                    { "element": "i", "message": "use Miranda <Text> instead" },
+                    { "element": "em", "message": "use Miranda <Text> instead" },
+                    { "element": "mark", "message": "use Miranda <Text> instead" },
+                    { "element": "small", "message": "use Miranda <Text> instead" },
+                    { "element": "h1", "message": "use Miranda <Text> instead" },
+                    { "element": "h2", "message": "use Miranda <Text> instead" },
+                    { "element": "h3", "message": "use Miranda <Text> instead" },
+                    { "element": "h4", "message": "use Miranda <Text> instead" },
+                    { "element": "h5", "message": "use Miranda <Text> instead" },
+                    { "element": "h6", "message": "use Miranda <Text> instead" },
+                    { "element": "input type='checkbox'", "message": "use Miranda <Checkbox> instead" },
+                    { "element": "label", "message": "use Miranda <Label> instead" },
+                    { "element": "a", "message": "use Miranda <Link> instead" },
+                    { "element": "input type='radio'", "message": "use Miranda <Radio> instead" },
+                    { "element": "select", "message": "use Miranda <Select> instead" },
+                    { "element": "table", "message": "use Miranda <Table> instead" },
+                    { "element": "input type='text'", "message": "use Miranda <TextField> instead" },
+                    { "element": "textarea", "message": "use Miranda <Textarea> instead" },
+                ] 
+            }
+        ]
+    }
+}
