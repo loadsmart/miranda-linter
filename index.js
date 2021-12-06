f3850067af0365b44d7881f5d0ba603bfbdc1c10
Juliana Souza <89201421+julianascs1@users.noreply.github.com>
2021-12-06 17:29:21 -0300

    Adding button rule

--- index.js
+++ index.js
@@ -30,12 +30,13 @@ module.exports = {
                     { "element": "h4", "message": "use Miranda <Text> instead" },
                     { "element": "h5", "message": "use Miranda <Text> instead" },
                     { "element": "h6", "message": "use Miranda <Text> instead" },
-                    { "element": "input", "message": "Use one of Miranda's input components like <TextField>, <Checkbox> or <Radio>  instead. For other non-supported types, disable this rule" },
+                    { "element": "input", "message": "Use one of Miranda's input components like <TextField>, <Checkbox>, <Radio> or <Button>  instead. For other non-supported types, disable this rule" },
                     { "element": "label", "message": "use Miranda <Label> instead" },
                     { "element": "a", "message": "use Miranda <Link> instead" },
                     { "element": "select", "message": "use Miranda <Select> instead" },
                     { "element": "table", "message": "use Miranda <Table> instead" },
                     { "element": "textarea", "message": "use Miranda <Textarea> instead" },
+                    { "element": "button", "message": "use Miranda <Button> instead" },
                 ] 
             }
         ]
