c4cb43b033f956745244c8ac3ace7000340b3c65
Bernardo Menescal Conde Mariano <berna@MacBook-Pro-de-Bernardo.local>
2021-12-20 17:16:12 -0300

    feat(lint): add more tags

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 module.exports = {
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+  rules: {
+    "react/forbid-elements": [
+      1,
+      {
+        forbid: [
+          { element: "button", message: "use Miranda <Button> instead" },
+          { element: "span", message: "use Miranda <Text> instead" },
+          { element: "p", message: 'use Miranda <Text as="p"> instead' },
+          { element: "b", message: 'use Miranda <Text as="b"> instead' },
+          {
+            element: "strong",
+            message: 'use Miranda <Text as="strong"> instead',
+          },
+          { element: "i", message: 'use Miranda <Text as="i"> instead' },
+          { element: "em", message: 'use Miranda <Text as="em"> instead' },
+          { element: "mark", message: 'use Miranda <Text as="mark"> instead' },
+          {
+            element: "small",
+            message: 'use Miranda <Text as="small"> instead',
+          },
+          { element: "h1", message: 'use Miranda <Text as="h1"> instead' },
+          { element: "h2", message: 'use Miranda <Text as="h2"> instead' },
+          { element: "h3", message: 'use Miranda <Text as="h3"> instead' },
+          { element: "h4", message: 'use Miranda <Text as="h4"> instead' },
+          { element: "h5", message: 'use Miranda <Text as="h5"> instead' },
+          { element: "h6", message: 'use Miranda <Text as="h6"> instead' },
+          {
+            element: "input",
+            message:
+              "use one of Miranda's input components like <TextField>, <Checkbox>, <Radio> or <Button> instead. For other non-supported types, disable this rule for this line",
+          },
+          { element: "textarea", message: "use Miranda <Textarea> instead" },
+          { element: "label", message: "use Miranda <Label> instead" },
+          { element: "a", message: "use Miranda <Link> instead" },
+          { element: "select", message: "use Miranda <Select> instead" },
+          { element: "table", message: "use Miranda <Table> instead" },
+          { element: "tr", message: "use Miranda <Table.Row> instead" },
+          { element: "td", message: "use Miranda <Table.Cell> instead" },
+          { element: "th", message: "use Miranda <Table.HeadCell> instead" },
+          { element: "tbody", message: "use Miranda <Table.Body> instead" },
+          { element: "thead", message: "use Miranda <Table.Head> instead" },
+          { element: "tfoot", message: "use Miranda <Table.Foot> instead" },
+          {
+            element: "caption",
+            message: "use Miranda <Table.Caption> instead",
+          },
+        ],
+      },
     ],
-    "plugins": [
-        "react"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 10,
-        "sourceType": "module"
-    },
-    "rules": {
-        "react/forbid-elements": [1, { 
-                "forbid": [
-                    { "element": "p", "message": "use Miranda <Text> instead" },
-                    { "element": "span", "message": "use Miranda <Text> instead" },
-                    { "element": "b", "message": "use Miranda <Text> instead" },
-                    { "element": "strong", "message": "use Miranda <Text> instead" },
-                    { "element": "i", "message": "use Miranda <Text> instead" },
-                    { "element": "em", "message": "use Miranda <Text> instead" },
-                    { "element": "mark", "message": "use Miranda <Text> instead" },
-                    { "element": "small", "message": "use Miranda <Text> instead" },
-                    { "element": "h1", "message": "use Miranda <Text> instead" },
-                    { "element": "h2", "message": "use Miranda <Text> instead" },
-                    { "element": "h3", "message": "use Miranda <Text> instead" },
-                    { "element": "h4", "message": "use Miranda <Text> instead" },
-                    { "element": "h5", "message": "use Miranda <Text> instead" },
-                    { "element": "h6", "message": "use Miranda <Text> instead" },
-                    { "element": "input", "message": "Use one of Miranda's input components like <TextField>, <Checkbox>, <Radio> or <Button>  instead. For other non-supported types, disable this rule" },
-                    { "element": "label", "message": "use Miranda <Label> instead" },
-                    { "element": "a", "message": "use Miranda <Link> instead" },
-                    { "element": "select", "message": "use Miranda <Select> instead" },
-                    { "element": "table", "message": "use Miranda <Table> instead" },
-                    { "element": "textarea", "message": "use Miranda <Textarea> instead" },
-                    { "element": "button", "message": "use Miranda <Button> instead" },
-                ] 
-            }
-        ]
-    }
-}
+  },
+};
